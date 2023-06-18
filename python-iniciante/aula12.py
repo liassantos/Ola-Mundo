561e0288e2fd3ffae8d2c501f59f2642cec20bfd
liassantos <134024865+liassantos@users.noreply.github.com>
2023-06-18 18:53:31 -0300

    Create aula12.py

---
+++ python-iniciante/aula12.py
@@ -0,0 +1,8 @@
+nome = str(input('Qual é o seu nome? '))
+if nome == 'Gustavo':
+    print('Que nome bonito!')
+elif nome== 'Pedro':
+    print('Seu nome é popular')
+else:
+    print('Seu nome é bem normal...')
+print(f'Tenha um bom dia {nome}')
