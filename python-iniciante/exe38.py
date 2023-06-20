177ee5bdd0186465e7cda5e32d88f168d8ead408
liassantos <134024865+liassantos@users.noreply.github.com>
2023-06-20 20:36:56 -0300

    Create exe38.py

---
+++ python-iniciante/exe38.py
@@ -0,0 +1,10 @@
+#Exe38 (Comparando números)
+
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+if n1 > n2:
+  print('O PRIMEIRO valor é MAIOR')
+elif n2 > n1:
+  print('O SEGUNDO valor é MAIOR')
+else:
+  print('Não existe valor maior, os dois são IGUAIS')
