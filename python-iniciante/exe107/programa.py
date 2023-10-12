e749cd91462ceae23d4a1e5dc4db5e8e661484c6
liassantos <134024865+liassantos@users.noreply.github.com>
2023-10-12 19:07:28 -0300

    Create programa.py

---
+++ python-iniciante/exe107/programa.py
@@ -0,0 +1,13 @@
+#metade
+#dobro
+#aumentar 10%
+#reduzir 13%
+
+from utilidadescev import moeda
+
+preço = float(input('Digite um preço: R$ '))
+# print(metade(preço, True))
+# print(dobro(preço, True))
+# print(aumentar(preço, 10, True))
+# print(diminuir(preço, 13, True))
+moeda.resumo(preço, 80, 35)
